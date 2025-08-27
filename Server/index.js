32c5c38999c38293a87c6cc9b686cbc4dd636a3a
oktapianir <ramadhanioktapiani@gmail.com>
2025-08-27 14:24:25 +0700

    install axios and connect between server and client

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mysql = require("mysql2");
 const cors = require("cors");
+const path = require("path");
+
 
 const app = express();
 const PORT = 5000;
@@ -7,9 +10,31 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express backend 🚀");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysql123",
+  database: "thrift_garage",
 });
+
+db.connect((err) => {
+  if (err){
+    console.error("DB connection failed:", err);
+    return;
+  }
+  console.log("Connected to MySql database");
+})
+
+app.get("/api/products", (req, res) => {
+  db.query("SELECT * FROM products", (err, results) => {
+    if (err) {
+      return res.status(500).json({error : err});
+    }
+    res.json(results);
+  });
+});
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
